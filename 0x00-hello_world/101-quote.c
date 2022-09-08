50b675a3625fd37b5da239c908a299b7655a2c15
MilkisaS <MilkisaS>
2022-09-08 04:08:39 -0700

    Parfur

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -10,9 +10,8 @@
 int main(void)
 
 {
-
-write(STDOUT_FILENO, "and that piece of art is useful\" - Dora Korpar,\2010-10- 19\n", 59);
-
-			return (1);
+		write(STDOUT_FILENO, "and that piece of art is useful
+				\" - Dora korpar, 2015-10-19\n", 59);	
+		return (1);
 
 }
