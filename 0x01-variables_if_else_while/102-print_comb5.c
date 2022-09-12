da8c6c4b17b70cad1f2a25d7fcab1ccda5bb6301
MilkisaS <MilkisaS>
2022-09-12 06:13:01 -0700

    Task 12

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,26 +8,21 @@
 
 int main(void)
 {
-	int digit1, digit2, digit3, digit4;
+	int digit1, digit2;
 
-	for (digit1 = 0; digit1 < 9; digit1++)
+	for (digit1 = 0; digit1 <= 98; digit1++)
 	{
-		for (digit2 = 0; digit2 < 9; digit2++)
+		for (digit2 = digit1 + 1; digit2 <= 99; digit2++)
 		{
-			for (digit3 = 0; digit3 < 9; digit3++)
-			{
-				for (digit4 = digit3 + 1; digit4 < 9; digit4++)
-				{
-					putchar((digit1 % 10) + '0');
-					putchar((digit2 % 10) + '0');
-					putchar(' ');
-					putchar((digit3 % 10) + '0');
-					putchar((digit4 % 10) + '0');
+			putchar((digit1 / 10) + '0');
+			putchar((digit1 % 10) + '0');
+			putchar(' ');
+			putchar((digit2 / 10) + '0');
+			putchar((digit2 % 10) + '0');
 
-					if (digit2 == 9)
-						continue;
-				}
-			}
+			if (digit1 == 98 && digit2 == 99)
+				continue;
+
 		}
 	}
 	putchar('\n');
