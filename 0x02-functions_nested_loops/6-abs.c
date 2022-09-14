7686ce39bf89fa7fc26787cc7680ae39eea12162
MilkisaS <MilkisaS>
2022-09-14 03:10:06 -0700

    Task 6

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _abs - It Prints the absolute value of number.
+ *
+ * @num: character to be verified.
+ * Return: Always positive value.
+ */
+
+int _abs(int num)
+{
+	if (num >= 0)
+		return (num);
+	else
+		return (-num);
+}
---
+++ 0x02-functions_nested_loops/6-main.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - checks the absolute value of the numbers.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int n;
+
+	n = _abs(-1);
+	printf("%d\n", n);
+	n = _abs(0);
+	printf("%d\n", n);
+	n = _abs(1);
+	printf("%d\n", n);
+	n = _abs(-98);
+	printf("%d\n", n);
+	return (0);
+}
