040b99d643d5ccab266efcbdc5914f3775201987
MilkisaS <MilkisaS>
2022-09-14 05:40:41 -0700

    Task 10

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * add - Is used to add up two integer numbers.
+ *
+ * @b: Character to be verified.
+ * @a: Character to be declared.
+ * Return: the sum of the integers
+ */
+
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the addition code.
+ *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+	int result;
+
+	result = add(89, 9);
+	printf("%d\n", result);
+	return (0);
+}
