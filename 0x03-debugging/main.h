44dbe1e6b372869f1ca7ee85035a9a8d6a61a1d3
MilkisaS <MilkisaS>
2022-09-26 12:43:39 -0700

    Task  4 and 5

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,8 +1,11 @@
 #ifndef MAIN.H
 #define MAIN.H
 
+
 int largest_number(int a, int b, int c);
+
 int convert_day(int month, int day);
+
 void print_remaining_days(int month, int day, int year);
 
 #endif
