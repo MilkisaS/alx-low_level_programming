91fe346246174d2e28f3dd817db6e9f53c649b3d
MilkisaS <MilkisaS>
2022-10-20 03:05:52 -0700

    correction on header files

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -2,7 +2,12 @@
 #define LISTS_H
 
 #include <stdlib.h>
-
+/**
+  * struct list_s - singly linked list
+  * @str: string - (malloc'ed string)
+  * @len: length of the string
+  * @next: points to the next node
+  */
 typedef struct list_s
 {
 	char *str;
